6acd09d56288c466b5c50144ea77892298be87e4
YU HENG GOH <22004880@siswa.um.edu.my>
2026-01-07 14:43:02 +0800

    Springboot setup and initial configuration

---
+++ wif3006-cbse-springboot/src/main/java/com/inventorymanagement/InventoryManagementApplication.java
@@ -0,0 +1,19 @@
+package com.inventorymanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class InventoryManagementApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(InventoryManagementApplication.class, args);
+    }
+
+    @GetMapping("/")
+    public String home() {
+        return "<h1>Success! Spring Boot is running.</h1>";
+    }
+}

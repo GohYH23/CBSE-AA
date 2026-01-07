42096b37ff39cee05b2f56708f94e9e036fd23e7
YU HENG GOH <22004880@siswa.um.edu.my>
2026-01-07 17:17:11 +0800

    project structure for both springboot and osgi

---
+++ wif3006-cbse-springboot/src/main/java/Customer/CustomerRepository.java
@@ -0,0 +1,9 @@
+package Customer;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+    // This interface gives you save(), findAll(), delete() automatically
+}

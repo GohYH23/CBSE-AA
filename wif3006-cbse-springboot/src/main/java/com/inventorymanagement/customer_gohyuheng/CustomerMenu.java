7165b90e29a598741e14441cfcca5de4390b836f
YU HENG GOH <22004880@siswa.um.edu.my>
2026-01-21 22:29:25 +0800

    fix: modify customer function for check sale order dependency and update test file for springboot

--- wif3006-cbse-springboot/src/main/java/com/inventorymanagement/customer_gohyuheng/CustomerMenu.java
+++ wif3006-cbse-springboot/src/main/java/com/inventorymanagement/customer_gohyuheng/CustomerMenu.java
@@ -265,8 +265,8 @@ public class CustomerMenu {
                     String delName = scanner.nextLine();
                     Optional<Customer> delOpt = customerService.getCustomerByName(delName);
                     if (delOpt.isPresent()) {
-                        customerService.deleteCustomer(delOpt.get().getId());
-                        System.out.println("✅ Customer deleted successfully.");
+                        String message = customerService.deleteCustomer(delOpt.get().getId());
+                        System.out.println(message);
                     } else {
                         System.out.println("❌ Customer trying deleted is not found.");
                     }

d956f051e265a98b7c9b62381abb43983aca2554
ericc326 <ericlee0485@gmail.com>
2026-01-22 18:54:04 +0800

    add edit function for both

--- wif3006-cbse-springboot/src/main/java/com/inventorymanagement/product_ericleechunkiat/repository/StockCountRepository.java
+++ wif3006-cbse-springboot/src/main/java/com/inventorymanagement/product_ericleechunkiat/repository/StockCountRepository.java
@@ -6,5 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface StockCountRepository extends MongoRepository<StockCount, String> {
-    // No custom logic needed for now, standard CRUD is enough
+
 }
